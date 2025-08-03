e44a74e4be7b803d8cce7126f4c7732cda0fca47
Revo3112 <revorahmat@gmail.com>
2025-08-03 12:35:24 +0700

    feat: update sitemap base URL for correct routing and remove outdated sitemap.xml file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://revo3112.vercel.app'
+  const baseUrl = 'https://revorahmat.vercel.app'
   const currentDate = new Date().toISOString()
 
   return [
